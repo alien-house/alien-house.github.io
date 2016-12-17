bf6581b61532921ee37d28705bc369506adbd892
alien-house <info@alien-house.com>
2016-12-16 19:47:47 -0800

    add part of something

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
 
 $(function(){
-		$('body').glassyWorms({
-			colors: ['#fff'],
-			useStyles  :true,
-			numParticles  :90, //250
-			tailLength :6, //12
-			maxForce   :5, // Moving force 8
-			gravity    :6, // 9.81
-			friction   :0.7 //0.75
-			// interval   :10
-		});
+	$('body').glassyWorms({
+		colors: ['#fff'],
+		useStyles  :true,
+		numParticles  :100, //250
+		tailLength :8, //12
+		maxForce   :5, // Moving force 8
+		gravity    :6, // 9.81
+		friction   :0.7 //0.75
+		// interval   :10
+	});
+});
 
 
-	// var windowHeight = $(window).height();
-	// $(".sec-intro").outerHeight(windowHeight);
-	// $(window).on('resize orientationchange', function(e) {
-	// 	$(".sec-intro").outerHeight($(window).height());
-	// });
+$(function(){
+	var windowHeight = $(window).height();
+	$(".intro-box").outerHeight(windowHeight);
+	$(window).on('resize orientationchange', function(e) {
+		$(".intro-box").outerHeight($(window).height());
+	});
+});
 
+$(function(){
 //https://github.com/mattboldt/typed.js/
 	$("#typed").typed({
-      strings: ["TO BUILD WEB SITE.",
+		strings: ["TO BUILD WEB SITE.",
 			"TO CREATE WEB SITE.",
-			"TO MAKE LOGO.",
-			"PURSUING my IDEAL."],
-      typeSpeed  : 0,
-			loop       :1
-  });
+			"TO DESIGN A LOGO.",
+			"something do related web."],
+		typeSpeed  : 0,
+		loop       :1
+	});
+});
 
 
+jQuery.easing.easeInOutCubic = function (e,f,a,h,g) {
+	if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a
+};
+$(function(){
+	$(".link_dmt").click(function(){
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000,'easeInOutCubic');
+		return false;
+	});
 });
