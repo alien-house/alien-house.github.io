0789a6c61d5e55fd9f59e319d21c595cf6993eaa
alien-house <info@alien-house.com>
2017-01-10 09:37:42 -0800

    changed token

--- js/script.js
+++ js/script.js
@@ -98,33 +98,59 @@ $(function() {
 
 
 var url_self = "https://api.instagram.com/v1/users/self/";
-
+var exis_accesstoken;
 $(function(){
 
-    // $.getJSON(url_self, function(data){
-    // 	console.log(data);
-    // });
-
-    //get a info from insta
-	var arg = new Object;
-	var pair = location.hash.substring(1).split('#');
-	for(var i = 0; pair[i]; i++) {
-	    var kv = pair[i].split('=');
-	    arg[kv[0]] = kv[1];
+    exis_accesstoken = localStorage.getItem("access_token")
+	if(!exis_accesstoken || exis_accesstoken == "undefined"){
+		console.log(exis_accesstoken);
+		//get a info from insta
+		var arg = new Object;
+		var pair = location.hash.substring(1).split('#');
+		for(var i = 0; pair[i]; i++) {
+		    var kv = pair[i].split('=');
+		    arg[kv[0]] = kv[1];
+		}
+		localStorage.setItem("access_token", arg['access_token']);
+	}else{
 	}
-
+	// console.log(exis_accesstoken);
 
     $.ajax({
-        url: url_self +"?access_token=" + arg['access_token'],
+        url: url_self +"?access_token=" + exis_accesstoken,
         method: 'get',
         dataType: 'jsonp',
         success: function(data) {
-        	if(data){
+        	console.log(data);
+        	if(data !== undefined){
     			$("#name").append(data.data.full_name);
         	}
         }
     });
 
+	$("#search-btn").on("click", function(){
+	    $.ajax({
+	        url: url_self +"?access_token=" + exis_accesstoken,
+	        method: 'get',
+	        dataType: 'jsonp',
+	        success: function(data) {
+	        	if(data){
+	    			$("#name").append(data.data.full_name);
+	        	}
+	        }
+	    });
+	});
+
+	$("#follow-btn").on("click", function(){
+	    $.ajax({
+	        url: url_self +"follows?access_token=" + exis_accesstoken,
+	        method: 'get',
+	        dataType: 'jsonp',
+	        success: function(data) {
+	        	console.log(data);
+	        }
+	    });
+	});
 
 
 });
